ef719eb1d53ef7c575a66a9e7dda1333e5892c08
Chetan Sharan <mr.chetansharan@gmail.com>
2024-07-30 23:50:18 +0530

    Updated GPT Run File

--- models/gpt/run.py
+++ models/gpt/run.py
@@ -2,11 +2,19 @@
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 # Load the fine-tuned model
 model = AutoModelForCausalLM.from_pretrained('./fine-tuned-model')
-tokenizer = AutoTokenizer.from_pretrained('./fine-tuned-model')
+tokenizer = AutoTokenizer.from_pretrained('./fine-tuned-tokenizer')
 
 # Initialize a pipeline for text generation
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 # Test the model
-response = generator("What is proxy?", max_length=50)
-print(response)
+print("")
+print("Hi, Welcome to Chetan's GPT Bot, you can ask any question and I will try to answer.")
+print("Type 'quit' or 'q' to exit")
+print("")
+question = input("Type your question here: ")
+while question.lower() != 'quit' and question.lower() != 'q':
+    response = generator(question, max_length=100)
+    print(response[0]['generated_text'])
+    print("")
+    question = input("You can ask more question here or type 'quit' or 'q' to exit:")
